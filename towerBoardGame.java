0b918be39eb82dc291795d87868f8c0776ca2947
Tryfon <tryfongav@gmail.com>
2025-01-15 18:25:44 +0200

    Changes by Panos

--- towerBoardGame.java
+++ towerBoardGame.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
 public class towerBoardGame {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int min=1;
+        int max=12;
+
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        int player = rand.nextInt((max - min)) + min;
+        int oddPlayer = 0;
+        int evenPlayer = 0;
+
+        System.out.println(player);
+
     }
 }
