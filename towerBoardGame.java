63235138a3036e698292eb40f5bbff4a88dc3604
Tryfon <tryfongav@gmail.com>
2025-01-16 16:16:21 +0200

    test

--- towerBoardGame.java
+++ towerBoardGame.java
@@ -39,7 +39,6 @@ public class towerBoardGame {
                 } else if (towers[0][1] == evenLimit) {
                     towers[0][1] = 0;
                     evenPlayerScore += 100;
-
                 }
             } else { //odd
                 System.out.println("Odd plays. ");
@@ -54,10 +53,21 @@ public class towerBoardGame {
                 }
             }
 
-            // Print current state of towers
-            System.out.println("ODD Towers: " + towers[0][0] + " (open: " + towerOpen[0][0] + "), " + towers[0][1] + " (open: " + towerOpen[0][1] + ")");
-            System.out.println("EVEN Towers: " + towers[1][0] + " (open: " + towerOpen[1][0] + "), " + towers[1][1] + " (open: " + towerOpen[1][1] + ")");
+
+            // TOWER STATS PRINT
+            System.out.println("EVEN Towers: " + towers[0][0] + " (open: " + towerOpen[0][0] + "), " + towers[0][1] + " (open: " + towerOpen[0][1] + ")");
+            System.out.println("ODD Towers: " + towers[1][0] + " (open: " + towerOpen[1][0] + "), " + towers[1][1] + " (open: " + towerOpen[1][1] + ")");
             System.out.println();
+
+
+        } //PLAYER POINTS COMPARISON
+        if (oddPlayerScore == evenPlayerScore) {
+            System.out.println("IT'S A TIE!");
+
+        } else if (oddPlayerScore > evenPlayerScore) {
+            System.out.println("ODD PLAYER WINS WITH : (" + oddPlayerScore + ") POINTS");
+        } else {
+            System.out.println("EVEN PLAYER WINS WITH : (" + evenPlayerScore + " POINTS)");
         }
     }
 
@@ -67,27 +77,30 @@ public class towerBoardGame {
         do {
             System.out.println("Choose a tower to place the number " + methodDice + ":");
             for (int i = 0; i < towers.length; i++) {
-                System.out.println((i + 1) + ". " + typeOfPlayer + "Tower" + (i + 1) + " (Current: " + typeOfPlayer + ", Open: " + towerOpen[i] + ")");
+                System.out.println((i + 1) + ". " + typeOfPlayer + "Tower " + (i + 1) + " (Current: " + towers[i] + ", Open: " + towerOpen[i] + ")");
+            }
 
-                System.out.println("Please select Tower 1 or 2 :");
-                userTowerChoice = inputs.nextInt() - 1;
+            System.out.println("Please select Tower 1 or 2 :");
+            userTowerChoice = inputs.nextInt() - 1;
 
-                if (userTowerChoice < 0 || userTowerChoice > 2) {
-                    System.out.println("***** Invalid input. Please try again. *****");
+            if (!towerOpen[userTowerChoice] == towerOpen[0]) {
+                System.out.println("***** Tower " + (userTowerChoice + 1) + " is closed. Please choose another Tower. *****");
+
+                if (!towerOpen[userTowerChoice] == towerOpen[1]) {
+                    //input method dice roll
+
                     userTowerChoice = -1;
                 }
+            } else if ((userTowerChoice < 0 || userTowerChoice > 2 || !towerOpen[userTowerChoice])) {
+                System.out.println("***** Invalid input. Please try again. *****");
+                userTowerChoice = -1;
             }
-        } while (userTowerChoice == -1);
+        }
+        while (userTowerChoice == -1);
         towers[userTowerChoice] += methodDice;
         if (towers[userTowerChoice] > limitedTowerScore) {
-            towers[userTowerChoice] = limitedTowerScore;
             towerOpen[userTowerChoice] = false;
             System.out.println("Tower " + (userTowerChoice + 1) + " is now closed.");
         }
-
     }
-
-
-        }
-
-
+}
