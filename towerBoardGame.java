7ebce2616ad269dc53e969ee4af4e8a153f0d568
Tryfon <tryfongav@gmail.com>
2025-01-16 17:16:56 +0200

    test

--- towerBoardGame.java
+++ towerBoardGame.java
@@ -1,3 +1,7 @@
+
+// This was made by Kostas Plassaras, Tryfonas Gavrilis, Panos Chologkitas and Stergios Fotoglou
+// Using the "Code with Me" feature provided by Intelij
+
 import java.util.*;
 
 public class towerBoardGame {
@@ -8,11 +12,12 @@ public class towerBoardGame {
 
         int oddLimit = 31;
         int evenLimit = 32;
-
+        //2D arrays with integer values for both Even and Odd towers.
         int[][] towers = {
                 {0, 0}, //Even Towers (1,2)
                 {0, 0}  //Odd Towers (1,2)
         };
+        //2D arrays with boolean values for both Even and Odd towers.
         boolean[][] towerOpen = {
                 {true, true}, //Even Towers Condition
                 {true, true}  //Odd Towers -//-
@@ -21,7 +26,7 @@ public class towerBoardGame {
         int oddPlayerScore = 0;
         int evenPlayerScore = 0;
 
-        while ((towerOpen[0][0]) || (towerOpen[0][1]) || towerOpen[1][0] || towerOpen[1][1]) {
+        while ((towerOpen[0][0]) || (towerOpen[0][1]) || towerOpen[1][0] || towerOpen[1][1]) { // Main game loop
 
 
 
@@ -30,19 +35,19 @@ public class towerBoardGame {
 
 
             if (mainDice % 2 == 0 && (towerOpen[0][0] || towerOpen[0][1])) {
-                System.out.println("Even plays. (" + evenPlayerScore + ")");
+                System.out.println("Even plays. (" + evenPlayerScore + " points)");
                 playerType = "EVEN ";
                 playerTurn(towers[0], towerOpen[0], evenLimit, mainDice, playerType);
 
-                if (towers[0][0] == evenLimit) { //even
+                if (towers[0][0] == evenLimit) { // Even's player turn
                     towers[0][0] = 0;
                     evenPlayerScore += 100;
                 } else if (towers[0][1] == evenLimit) {
                     towers[0][1] = 0;
                     evenPlayerScore += 100;
                 }
-            } else if (towerOpen[1][0] || towerOpen[1][1]){ //odd
-                System.out.println("Odd plays. (" + oddPlayerScore + ")");
+            } else if (towerOpen[1][0] || towerOpen[1][1]){ //Odd's player turn
+                System.out.println("Odd plays. (" + oddPlayerScore + " points )");
                 playerType = "ODD ";
                 playerTurn(towers[1], towerOpen[1], oddLimit, mainDice, playerType);
                 if (towers[1][0] == oddLimit) {
@@ -79,7 +84,8 @@ public class towerBoardGame {
             System.out.println("EVEN PLAYER WINS WITH : (" + evenPlayerScore + " POINTS)");
         }
     }
-
+    // Method indicating the Tower selection depending on whose turn it is.
+    //Cannot make sure that the user enters only 1 or 2. Invalid input finishes the game.
     public static void playerTurn(int[] towers, boolean[] towerOpen, int limitedTowerScore, int methodDice, String typeOfPlayer) {
         Scanner inputs = new Scanner(System.in);
         int userTowerChoice = -1;
@@ -93,15 +99,15 @@ public class towerBoardGame {
             userTowerChoice = inputs.nextInt() - 1;
 
             if (!towerOpen[userTowerChoice] == towerOpen[0]) {
-                System.out.println("***** Tower " + (userTowerChoice + 1) + " is closed. Please choose another Tower. *****");
+                System.out.println(" Tower " + (userTowerChoice + 1) + " is closed. Please choose another Tower. ");
 
                 if (!towerOpen[userTowerChoice] == towerOpen[1]) {
-                    //input method dice roll
+
 
                     userTowerChoice = -1;
                 }
             } else if ((userTowerChoice > 2 || !towerOpen[userTowerChoice])) {
-                System.out.println("***** Invalid input. Please try again. *****");
+                System.out.println(" Invalid input. Please try again. ");
                 userTowerChoice = -1;
             }
         }
